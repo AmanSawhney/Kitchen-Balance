24946f7814c7a1e866464f102c854d51f922082d
Jorrie Brettin <jorriebrettin@gmail.com>
2015-07-18 15:57:21 -0400

    Physics completed

--- Untitled.spritebuilder/Source/MainScene.swift
+++ Untitled.spritebuilder/Source/MainScene.swift
@@ -10,11 +10,20 @@ class MainScene: CCNode {
   weak var hand: CCNode!
   weak var object: CCSprite!
   weak var gamePhysicsNode: CCPhysicsNode!
+  weak var scoreLabel: CCLabelTTF!
   var currentTouchLocation: CGPoint!
   var pivot: CCPhysicsJoint!
+  var score: Double = 0 {
+    didSet{
+      scoreLabel.string = "\(Int(score))"
+    }
+  }
+  let scorePerUpdate = 32.0
+//  var 
+  var rotationMultiplier: Double = 1
   var done = false {
     didSet {
-      var shitScreen = CCBReader.load("ShitScreen") as! ShitScreen
+      var shitScreen = CCBReader.load("ShitScreen", owner:self) as! ShitScreen
       shitScreen.position.x = -self.contentSize.width
       self.addChild(shitScreen)
     }
@@ -26,7 +35,7 @@ class MainScene: CCNode {
   
   func didLoadFromCCB() {
     
-    gamePhysicsNode.debugDraw = true
+//    gamePhysicsNode.debugDraw = true
     userInteractionEnabled = true
   }
   
@@ -53,13 +62,22 @@ class MainScene: CCNode {
     hand = CCBReader.load("Objects/Hand")
     hand.scale = 0.6
     hand.position = ccp(screenWidth/2, 0)
+//    hand.physicsBody.type = CCPhysicsBodyType(rawValue: UInt(1))!
     object.position = ccp(screenWidth/2, hand.contentSizeInPoints.height * CGFloat(hand.scale) * (CGFloat(1) - hand.anchorPoint.y))
-
-    gamePhysicsNode.addChild(object)
+    
+    hand.zOrder = 2
+    object.zOrder = 1
+    
     gamePhysicsNode.addChild(hand)
-    pivot = CCPhysicsJoint(pivotJointWithBodyA: object.physicsBody, bodyB: hand.physicsBody, anchorA: ccp(30, 0))
+    gamePhysicsNode.addChild(object)
+    
+    pivot = CCPhysicsJoint(pivotJointWithBodyA: object.physicsBody, bodyB: hand.physicsBody, anchorA: ccp(20, 0))
+//    pivot = CCPhysicsJoint(distanceJointWithBodyA: object.physicsBody, bodyB: hand.physicsBody, anchorA: ccp(0,0), anchorB: object.position, minDistance: CGFloat(0), maxDistance: CGFloat(0))
     pivot.collideBodies = false
-    object.physicsBody.applyImpulse(ccp(0,10))
+    
+    var randomRotation = Double(arc4random_uniform(2)) + 1.0
+    object.rotation = Float(arc4random_uniform(2) == 1 ? randomRotation : -randomRotation)
+//    println(object.rotation)
     
   }
   
@@ -67,14 +85,16 @@ class MainScene: CCNode {
     currentTouchLocation = touch.locationInWorld()
   }
   override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-    hand.position.x = hand.position.x - (currentTouchLocation.x - touch.locationInWorld().x)
-    println(currentTouchLocation == touch.locationInWorld())
-    println("This touch Location = \(touch.locationInWorld().x)")
-    println("Previous touch Location = \(currentTouchLocation.x)")
-
+    hand.position.x = CGFloat(clampf(Float(hand.position.x - (currentTouchLocation.x - touch.locationInWorld().x)), Float(0.0), Float(screenWidth)))
     currentTouchLocation = touch.locationInWorld()
   }
   override func update(delta: CCTime) {
+    hand.position.y = 0
+//    object.physicsBody.velocity.y = 0
+    
+    
+    score += rotationMultiplier * scorePerUpdate
+    
     //        println(Int(pin.rotation))
     if Int(object.rotation) <= -130 || (Int(object.rotation) >= 110) {
       if !done {
@@ -84,14 +104,19 @@ class MainScene: CCNode {
       //      var mainScene : CCScene =  CCBReader.loadAsScene("Easy")
       //      CCDirector.sharedDirector().replaceScene(mainScene)
     }
-    if currentTouchLocation != nil {
-      var random = arc4random_uniform(300)
-      if hand.position.x > CCDirector.sharedDirector().viewSize().width/2 {
-        object.physicsBody.applyImpulse(ccp(-CGFloat(random),0))
-      } else if hand.position.x < CCDirector.sharedDirector().viewSize().width/2 {
-        object.physicsBody.applyImpulse(ccp(CGFloat(random),0))
-      }
-    }
+//    if currentTouchLocation != nil {
+//      var random = arc4random_uniform(300)
+//      if hand.position.x > CCDirector.sharedDirector().viewSize().width/2 {
+//        object.physicsBody.applyImpulse(ccp(-CGFloat(random),0))
+//      } else if hand.position.x < CCDirector.sharedDirector().viewSize().width/2 {
+//        object.physicsBody.applyImpulse(ccp(CGFloat(random),0))
+//      }
+//    }
+  }
+  
+  func restart(){
+    var playScene = CCBReader.loadAsScene("MainScene")
+    CCDirector.sharedDirector().replaceScene(playScene)
   }
   
 }
