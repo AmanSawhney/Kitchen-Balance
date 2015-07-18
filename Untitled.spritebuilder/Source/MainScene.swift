c86fbfbb6fa0609d50c901e65a59a901aafd3716
Jorrie Brettin <jorriebrettin@gmail.com>
2015-07-18 14:19:32 -0400

    Touch functionality updated

--- Untitled.spritebuilder/Source/MainScene.swift
+++ Untitled.spritebuilder/Source/MainScene.swift
@@ -10,7 +10,7 @@ class MainScene: CCNode {
   weak var hand: CCNode!
   weak var object: CCSprite!
   weak var gamePhysicsNode: CCPhysicsNode!
-  weak var currentTouch: CCTouch!
+  var currentTouchLocation: CGPoint!
   var pivot: CCPhysicsJoint!
   var done = false {
     didSet {
@@ -19,6 +19,7 @@ class MainScene: CCNode {
       self.addChild(shitScreen)
     }
   }
+  
   
   var screenWidth = UIScreen.mainScreen().bounds.width
   var screenHeight = UIScreen.mainScreen().bounds.height
@@ -50,27 +51,28 @@ class MainScene: CCNode {
     }
 
     hand = CCBReader.load("Objects/Hand")
-    hand.scale = 0.75
+    hand.scale = 0.6
     hand.position = ccp(screenWidth/2, 0)
-    
     object.position = ccp(screenWidth/2, hand.contentSizeInPoints.height * CGFloat(hand.scale) * (CGFloat(1) - hand.anchorPoint.y))
 
-    
-    gamePhysicsNode.addChild(hand)
     gamePhysicsNode.addChild(object)
-    
-    pivot = CCPhysicsJoint(pivotJointWithBodyA: object.physicsBody, bodyB: hand.physicsBody, anchorA: ccp(object.position.x/4,0))
+    gamePhysicsNode.addChild(hand)
+    pivot = CCPhysicsJoint(pivotJointWithBodyA: object.physicsBody, bodyB: hand.physicsBody, anchorA: ccp(30, 0))
     pivot.collideBodies = false
     object.physicsBody.applyImpulse(ccp(0,10))
     
   }
   
   override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-    
+    currentTouchLocation = touch.locationInWorld()
   }
   override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-    hand.position.x = touch.locationInWorld().x
-    currentTouch = touch
+    hand.position.x = hand.position.x - (currentTouchLocation.x - touch.locationInWorld().x)
+    println(currentTouchLocation == touch.locationInWorld())
+    println("This touch Location = \(touch.locationInWorld().x)")
+    println("Previous touch Location = \(currentTouchLocation.x)")
+
+    currentTouchLocation = touch.locationInWorld()
   }
   override func update(delta: CCTime) {
     //        println(Int(pin.rotation))
@@ -82,7 +84,7 @@ class MainScene: CCNode {
       //      var mainScene : CCScene =  CCBReader.loadAsScene("Easy")
       //      CCDirector.sharedDirector().replaceScene(mainScene)
     }
-    if currentTouch != nil {
+    if currentTouchLocation != nil {
       var random = arc4random_uniform(300)
       if hand.position.x > CCDirector.sharedDirector().viewSize().width/2 {
         object.physicsBody.applyImpulse(ccp(-CGFloat(random),0))
